94c1957d986f02ee8d362e0b310e777589644651
amit.shalev <amit.shalev@hazera.com>
2019-11-27 13:34:12 +0200

    breaking down draw annotations to smaller modular functions

--- image_processing/core.py
+++ image_processing/core.py
@@ -6,13 +6,11 @@ def draw_annoations(img,annots):
     img : np.array
     annots: maagad format
     """
-    import cv2
     font = cv2.FONT_HERSHEY_PLAIN
     for annot in annots:
         if annot['deleted']!=1:
             if annot['annotation_type']=='polygon':
-                contour=np.array([(x['x'],x['y']) for x in annot['annotations']],dtype=np.int32)
-                cv2.drawContours(img,[contour] ,0, (0,0,0),18)
+                img = draw_polygon(img,annot['annotations'])
             elif annot['annotation_type']=='oval':
                 print(annot['annotations'])
                 box=[(annot['annotations']['x'],annot['annotations']['y']),
@@ -24,3 +22,12 @@ def draw_annoations(img,annots):
 
     fig, ax = plt.subplots(figsize=(10, 22))
     ax.imshow(img, interpolation='nearest')
+
+
+def draw_polygon(img,ploygon,x='x',y='y'):
+    '''
+    adds a polygon to the img and returns the new img
+    polygon= [{'x':23,'y':531}]
+    '''
+    contour = np.array([(point[x],point[y]) for point in ploygon,dtype=np.int32)
+    cv2.drawContours(img,[contour] ,0, (0,0,0),18)
