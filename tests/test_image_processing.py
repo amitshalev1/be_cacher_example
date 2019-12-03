6c0d30039df2d31d3328c232010eb5506a0f76e9
amit.shalev <amit.shalev@hazera.com>
2019-12-03 14:20:30 +0200

    fixed imports on tests

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -1,7 +1,7 @@
-from image_processing import crop
-
 def test_crop():  
+    from image_processing import crop
+    import numpy as np
     test_img = (255*np.random.random((100,100))).astype('uint8')
     test_img[11:40,20:40]= 0
     cont =[[21,12],[39,12],[39,39],[21,12]]    
-    assert (crop(img,np.array(cont))==0).all()    
+    assert (crop(test_img,np.array(cont))==0).all()    
